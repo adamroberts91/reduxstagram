92a90435230e362a704d82ef0745dafcef770f5f
Adam Roberts <adam.j.roberts91@gmail.com>
2018-06-20 01:28:46 +0100

    first commit

---
+++ client/components/Main.js
@@ -0,0 +1,19 @@
+// https://courses.wesbos.com/account/access/5b298a3af9ec296a73d76742/view/195950239
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>
+                    <Link to="/">Reduxstagram</Link>
+                </h1>
+                {React.cloneElement(this.props.children, this.props)}
+            </div>
+        );
+    }
+});
+
+export default Main;
---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
